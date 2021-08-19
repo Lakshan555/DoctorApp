11764cd8dc03dac6f8f01eed82a7855042255fbe
onlineipl18@gmail.com <onlineipl18@gmail.com>
2021-08-19 10:49:47 +0530

    developed the interfaces

---
+++ app/src/main/java/com/example/myapplication/Create_D_Profile.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Create_D_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_doctor_profile);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Notice_View.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notice_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice__view);
+    }
+}
